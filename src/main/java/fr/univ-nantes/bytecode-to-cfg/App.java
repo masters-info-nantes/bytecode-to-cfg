9bcc800c790d1aedf58dbe5e06209104fe46fcaa
Anthony Pena <anthony.pena@outlook.fr>
2014-11-07 14:48:16 +0100

    create maven project

---
+++ src/main/java/fr/univ-nantes/bytecode-to-cfg/App.java
@@ -0,0 +1,13 @@
+package fr.univ-nantes.bytecode-to-cfg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
