42b104cd26eb27fb36c9b52da75b71518ae485da
jbardon <bardonjeremy1@gmail.com>
2014-11-13 18:40:23 +0100

    Add dependence with control-flow-graph project

--- src/main/java/fr/univnantes/bytecodetocfg/Main.java
+++ src/main/java/fr/univnantes/bytecodetocfg/Main.java
@@ -1,9 +1,15 @@
 package fr.univnantes.bytecodetocfg;
 
 import java.io.InputStream;
+import java.util.LinkedList;
 
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassVisitor;
+
+import fr.univnantes.controlflowgraph.Arc;
+import fr.univnantes.controlflowgraph.Condition;
+import fr.univnantes.controlflowgraph.Instruction;
+import fr.univnantes.controlflowgraph.Node;
 
 /*
  * Run class analyzer on class which contains
@@ -16,5 +22,58 @@ public class Main {
         InputStream in = AnalyzedClass.class.getResourceAsStream("AnalyzedClass.class");
         ClassReader classReader = new ClassReader(in);
         classReader.accept(new ClassAnalyzer(), 0);
+        
+        graphtest();
     }
+    
+    public static void graphtest() {
+		System.out.println("Hello World!");
+		Node origin,nA,nB,nC,nD;
+		Arc a;
+		
+		/* Example :
+		 * 
+		 * x = 8;
+		 * if(x > 8) {
+		 *	  y = 6
+		 * } else if(x == 8 ) {
+		 *	  y = 7
+		 * } else {
+		 *    y = 1;
+		 * }
+		 * 
+		 */
+		
+		origin = new Instruction("x = 8");
+		
+		nA = new Condition();
+		a = new Arc(" ",nA);
+		origin.addArc(a);
+		
+		nB = new Instruction("y = 6");
+		a = new Arc("x > 8",nB);
+		nA.addArc(a);
+		
+		nC = new Instruction("y = 7");
+		a = new Arc("x == 8",nC);
+		nA.addArc(a);
+		
+		nD = new Instruction("y = 1");
+		a = new Arc(" ",nD);
+		nA.addArc(a);
+		
+		// Print example
+		System.out.println("Node["+origin+"]");
+		LinkedList<Node> queue = new LinkedList<Node>();
+		queue.add(origin);
+		Node cur = null;
+		System.out.println("size ="+queue.size());
+		while(queue.size() > 0) {
+			cur = queue.remove();
+			System.out.println(cur.toString());	
+			for(Arc arc : cur.getArcs()) {
+				queue.add(arc.getNext());
+			}
+		}
+	}
 }
